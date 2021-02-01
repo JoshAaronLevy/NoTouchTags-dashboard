db85049a0559c7aae908e86bb29d6a1a08017ee4
Josh Levy <joshaaronlevy@gmail.com>
2021-01-31 19:04:27 -0700

    Implementing Parse

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  PARSE_APP_ID: 'w17nTT0TMtIjiH1qzS4r6zAGig6EOAHNpncny7VF',
+  PARSE_JS_KEY: 'kBlbaoq8fd2KblWBNEApECYIEPiCF8CqZyWvXYK2',
+  serverURL: 'https://parseapi.back4app.com'
 };
